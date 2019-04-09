1b09eacd90a42a87e27fd86eb5f3771a7ff1f523
zhurunfeng <1213915926@qq.com>
2019-04-09 16:17:56 +0800

    0.1

---
+++ resources/views/common/layouts.blade.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html lang="zh-cn">
+<head>
+    <meta charset="UTF-8">
+    <title>工具集合</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+
+            <a href="index.html" class="navbar-brand logo"><img src="img/logo.png" alt="瓢城企训网"></a>
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse" id="navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="active"><a href="index.html"><span class="glyphicon glyphicon-home"></span> 首页</a></li>
+                <li><a href="###" class="testajax"><span class="glyphicon glyphicon-list"></span> 测试</a></li>
+                <li><a href="###"><span class="glyphicon glyphicon-question-sign"></span> 关于</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="jumbotron">
+    <div class="container">
+        <hgroup>
+            <h1>工具</h1>
+            <h4>定制工具...</h4>
+        </hgroup>
+    </div>
+</div>
+
+<div id="case">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <div class="thumbnail">
+                    <!-- <img src="img/case1.jpg" alt=""> -->
+                    <div class="caption">
+                        <h4>字符串长度（字数）</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <div class="thumbnail">
+                    <!-- <img src="img/case2.jpg" alt=""> -->
+                    <div class="caption">
+                        <h4>转码（各种格式互相转换）</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <div class="thumbnail">
+                    <!-- <img src="img/case3.jpg" alt=""> -->
+                    <div class="caption">
+                        <h4>处理excel（褚嘉惠定制）</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <div class="thumbnail">
+                    <!-- <img src="img/case4.jpg" alt=""> -->
+                    <div class="caption">
+                        <h4>进制转换</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <div class="thumbnail">
+                    <!-- <img src="img/case1.jpg" alt=""> -->
+                    <div class="caption">
+                        <h4>吃什么</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <div class="thumbnail">
+                    <!-- <img src="img/case1.jpg" alt=""> -->
+                    <div class="caption">
+                        <h4>计算公式（王璐定制）</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <div class="thumbnail">
+                    <!-- <img src="img/case1.jpg" alt=""> -->
+                    <div class="caption">
+                        <h4>kindle传书</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer id="footer">
+    <div class="container">
+        <p>工具定制 | 合作事宜 | 版权投诉</p>
+        <p>苏ICP 备12345678. © 2016-2019 xxxx. Powered by Bootstrap.</p>
+    </div>
+</footer>
+
+<script src="{{ asset('js/jquery.min.js') }}"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+
+</body>
+</html>
